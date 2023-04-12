4bad45466eda5575bda420045a05f1f78340474b
abplocker <nguyenquangthien7100@gmail.com>
2023-04-12 22:09:46 +0700

    Add cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 
 //Cấu hình body-parser
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 // Các routers
 
 require('./app/routers/user.router')(app);
