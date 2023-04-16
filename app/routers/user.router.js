0e0a6cefe756d7978edd3858524d5a474a0c6c7c
abplocker <nguyenquangthien7100@gmail.com>
2023-04-17 02:17:18 +0700

    Update

--- app/routers/user.router.js
+++ app/routers/user.router.js
@@ -2,6 +2,8 @@ module.exports = function (router) {
 
 	var userController = require('../controllers/user.controller');
 
+	var JWT = require('../config/security');
+
 	router.get('/user/all', userController.get_all);
 
 	router.get('/user/:id', userController.getById);
@@ -12,4 +14,18 @@ module.exports = function (router) {
 
 	router.put('/user/update_info',userController.update_info);
 	
+	// Test jwt
+	router.get('/token', async function (req, res){
+		const token = await JWT.createToken(req.body);
+		res.send({token: token})
+	});
+
+	router.get('/check_token', async function (req, res){
+		try {
+		const check = await JWT.checkToken(req.body);
+		res.send({check: check})
+		} catch (error) {
+            res.send({error: error})
+        }
+	});
 };
